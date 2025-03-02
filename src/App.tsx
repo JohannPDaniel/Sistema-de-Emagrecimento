fdd9d867c402955806892ee9d0d595c3c0a710e5
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2025-03-02 19:26:48 -0300

    Fazendo o AppBar da LP

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { GlobalStyle } from "./config/global/GlobalStyle";
-import AppRoutes from "./config/routes/AppRoutes";
+import { ThemeProvider } from '@mui/material';
+import { GlobalStyle } from './config/global/GlobalStyle';
+import AppRoutes from './config/routes/AppRoutes';
+import theme from './config/themes/theme';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { persistor, store } from './config/store';
 
 function App() {
-  return (
-    <>
-        <AppRoutes />
-        <GlobalStyle />
-    </>
-  );
+	return (
+		<Provider store={store}>
+			<PersistGate
+				loading={null}
+				persistor={persistor}>
+				<ThemeProvider theme={theme}>
+					<AppRoutes />
+					<GlobalStyle />
+				</ThemeProvider>
+			</PersistGate>
+		</Provider>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/LandingPage/AppBarMui.tsx
@@ -0,0 +1,69 @@
+import {
+	AppBar,
+	Button,
+	Container,
+	Stack,
+	Toolbar,
+	Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import theme from "../../config/themes/theme";
+
+export const AppBarMui = () => {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	if (typeof window !== 'undefined') {
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 50) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		});
+	}
+
+	return (
+		<AppBar
+			position='fixed'
+			color='default'
+			elevation={isScrolled ? 20 : 0}
+			sx={{
+				backgroundColor: 'white',
+				transition: 'all 0.3s ease',
+			}}>
+			<Container>
+				<Toolbar>
+					<Typography
+						variant='h6'
+						component='div'
+						sx={{
+							flexGrow: 1,
+							fontWeight: 700,
+							color: theme.palette.primary.main,
+						}}>
+						EmagreceJá
+					</Typography>
+					<Stack
+						direction='row'
+						spacing={2}>
+						<Button
+							component={RouterLink}
+							to='/login'
+							variant='contained'
+							color='primary'>
+							Login
+						</Button>
+						<Button
+							component={RouterLink}
+							to='/login'
+							variant='contained'
+							color='primary'>
+							Começar Agora
+						</Button>
+					</Stack>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+};
